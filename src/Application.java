9b06f4d43b69e23b85c002f0938418b5e10a3096
Max Henderson <maxwell.blair.henderson@gmail.com>
2018-11-27 00:50:56 -0800

    Load method fully implemented. It will load from the .txt files that are now included in the folder. Just need to make sure the DB is empty before running load. Feel free to add more lines to the .txt files, just be sure to pay attention to foreign key constraints. Implemented method to return available rooms. Once I have the outline for the menus, I can start implementing methods to carry out menu options. Should be pretty easy.

--- src/Application.java
+++ src/Application.java
@@ -11,13 +11,13 @@ public class Application {
         try {
             DriverManager.registerDriver(new Driver());
             Connection conn = DriverManager.getConnection(dbURL, "student", "password");
-            loadData("test.txt", conn); //Need to write file loader method
-
+            //loadData("test.txt", conn); //Need to write file loader method
+            checkAvailability(conn);
         } catch (SQLException ex){
             System.out.println(ex);
         }
 
-        
+
 
     }
 
@@ -30,24 +30,51 @@ public class Application {
     \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
      */
     static void loadData(String inputFile, Connection conn){
-        File file = new File(inputFile);
-        Scanner scan = null;
-        try{
-            scan = new Scanner(file);
-            while(scan.hasNextLine()){
-                String line = scan.nextLine();
-                String query = "INSERT INTO User VALUES (" + line + ");";
-                System.out.println(query);
-                PreparedStatement p = conn.prepareStatement(query);
-                p.clearParameters();
-                p.execute();
+        String[] tables = {"Person", "Employee","StudentAlumni", "Administrator","Maintainence", "Applicant",
+                "Resident","RoomType","Room","Application","MaintenanceRequest"};
+        for (String table : tables) {
+            System.out.println("Loading " + table);
+            File file = new File(table + ".txt");
+            Scanner scan = null;
+            try {
+                scan = new Scanner(file);
+                while (scan.hasNextLine()) {
+                    String line = scan.nextLine();
+                    String query = "INSERT INTO " + table + " VALUES (" + line + ");";
+                    //System.out.println(query);
+                    PreparedStatement p = conn.prepareStatement(query);
+                    p.clearParameters();
+                    p.execute();
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found.");
+            } catch (SQLException e) {
+                System.out.println(e);
             }
-        } catch(FileNotFoundException e) {
-            System.out.println("File not found.");
-        } catch(SQLException e){
-            System.out.println(e);
+            System.out.println("File " + table + " is loaded");
         }
-        System.out.println("File " + inputFile +  " is loaded");
+    }
+
+    static void checkAvailability(Connection conn){
+        try{
+            String query = "SELECT TypeName, RoomNum, BuildingNum, Village, Address FROM RoomType, Room WHERE RoomStatus=0;";
+            PreparedStatement p = conn.prepareStatement(query);
+            p.clearParameters();
+            ResultSet r = p.executeQuery();
+            System.out.println("Available rooms:");
+            System.out.println("--------------------------------------------------------");
+            while (r.next()) {
+                String rType = r.getString(1);
+                String rNum = r.getString(2);
+                String bNum = r.getString(3);
+                String village = r.getString(4);
+                String address = r.getString(5);
+                System.out.println("ROOM TYPE: " + rType + "\nVILLAGE: " + village + "\nADDRESS: " + address);
+                System.out.println("--------------------------------------------------------");
+            }
+        }catch(SQLException ex){
+            System.out.println(ex);
+        }
     }
 }
 
