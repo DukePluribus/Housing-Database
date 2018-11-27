c24511379ebf65dd02795a7d623c917ee496d8fc
Max Henderson <maxwell.blair.henderson@gmail.com>
2018-11-26 17:23:06 -0800

    Added Load Method
    Only implemented adding Users. Need to update it to work with new Schema and shit

--- src/Application.java
+++ src/Application.java
@@ -10,28 +10,43 @@ public class Application {
         String dbURL = "jdbc:mysql://localhost:3306/Dorms?serverTimezone=UTC&useSSL=TRUE";
         try {
             DriverManager.registerDriver(new Driver());
-            Connection conn = DriverManager.getConnection(url, "student", "password");
+            Connection conn = DriverManager.getConnection(dbURL, "student", "password");
+            loadData("test.txt", conn); //Need to write file loader method
 
         } catch (SQLException ex){
             System.out.println(ex);
         }
-        loadData("test.txt"); //Need to write file loader method
 
-
-
-
+        
 
     }
 
 
     /*
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    All the helper methods are below here
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+    \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+    |||||    All the helper methods are below here          |||||
+    /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+    \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
      */
-    static void loadData(String inputFile){
+    static void loadData(String inputFile, Connection conn){
+        File file = new File(inputFile);
+        Scanner scan = null;
+        try{
+            scan = new Scanner(file);
+            while(scan.hasNextLine()){
+                String line = scan.nextLine();
+                String query = "INSERT INTO User VALUES (" + line + ");";
+                System.out.println(query);
+                PreparedStatement p = conn.prepareStatement(query);
+                p.clearParameters();
+                p.execute();
+            }
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found.");
+        } catch(SQLException e){
+            System.out.println(e);
+        }
         System.out.println("File " + inputFile +  " is loaded");
     }
 }
